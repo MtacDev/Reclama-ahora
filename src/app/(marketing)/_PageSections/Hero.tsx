92778bfdd3d906e8fdf9341a8f26b454a10e575b
Mario Tapia <mtac@MacBook-Pro-de-Mario.local>
2024-09-11 01:39:26 -0300

    - Added search bar

--- src/app/(marketing)/_PageSections/Hero.tsx
+++ src/app/(marketing)/_PageSections/Hero.tsx
@@ -1,39 +1,40 @@
-import { cn } from '@/lib/utils/helpers';
-import Link from 'next/link';
-import { buttonVariants } from '@/components/ui/Button';
+import { Button } from '@/components/ui/Button';
 import './hero.scss'
+import { Input } from '@/components/ui/Input';
 
 export default function Hero() {
   return (
     <div className='hero-section'>
       <section className="py-18 px-4">
         <div className="flex flex-col items-center gap-4 text-center" >
-         <div className="title-style">
+         <div className="bg-black bg-opacity-80">
           <div className='py-8'>
-              <h1 className=" text-3xl md:text-7xl md:max-w-[54rem] " >
+              <h1 className="text-3xl md:text-7xl md:max-w-[54rem] text-white" >
                 Reclama Ahora! Tus derechos
               </h1>
               <div className="resume-text"> 
-                <h4 className="max-w-[42rem] sm:text-xm sm:leading-6 resume-text">
+                <h4 className="max-w-[42rem] sm:text-xm sm:leading-6 resume-text text-white">
                 Los derechos del consumidor en Chile garantizan protección, información, seguridad en productos y servicios, y la posibilidad de reclamar y exigir reparaciones.
                 </h4>
               </div>
               
-              <div className="space-x-4">
-                <Link href="/login" className={cn(buttonVariants({ size: 'lg' }))}>
-                  Get Started
-                </Link>  
-                <Link
-                  href="/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className={cn(buttonVariants({ variant: 'ghost', size: 'lg' }))}
-                >
-                  Learn More <span aria-hidden="true">→</span>
-                </Link>
-            </div>
+              
             </div>
           </div>
+          <div className="mt-6 sm:flex max-w-[42rem] ">
+                <Input
+                className="w-96 h-12 p-2"
+                  type="text"
+                  name="company-search"
+                  autoComplete="email"
+                  placeholder="Busca la Empresa, Servicio o Producto"
+                />
+                <div className="ml-4">
+                  <Button variant="secondary" type="submit" className="h-12">
+                    Empezar Reclamo
+                  </Button>
+                </div>
+              </div>
         </div>
       </section>
     </div>
